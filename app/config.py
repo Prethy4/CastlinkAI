406890cb4a1b902a1d7166c52ebae23e78bb7870
Tanjum Amin Prethy <tanjum@joinventureai.com>
2026-07-13 14:51:52 +0600

    media load: app/config.py

--- app/config.py
+++ app/config.py
@@ -7,6 +7,7 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 DATABASE_URL = os.getenv("DATABASE_URL", "DATABASE_LOCAL_URL")
 OPENAI_EMBEDDING_MODEL = "text-embedding-3-small"
+BASE_URL = os.getenv("BASE_URL")
 OPENAI_CHAT_MODEL = "gpt-5.1"  #gpt-5-mini
 # JWT Authentication
 JWT_SECRET_KEY=os.getenv("JWT_SECRET_KEY")
