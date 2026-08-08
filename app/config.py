c3a57ab263758e6a1826eef13eac50f3d6907807
Tanjum Amin Prethy <tanjum@joinventureai.com>
2026-08-08 09:58:26 +0600

    Image url & role response fix: app/config.py

--- app/config.py
+++ app/config.py
@@ -8,6 +8,7 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 DATABASE_URL = os.getenv("DATABASE_URL", "DATABASE_LOCAL_URL")
 OPENAI_EMBEDDING_MODEL = "text-embedding-3-small"
 BASE_URL = os.getenv("BASE_URL")
+BASE_URL_BACKEND = os.getenv("BASE_URL_BACKEND")
 OPENAI_CHAT_MODEL = "gpt-5.1"  #gpt-5-mini
 # JWT Authentication
 JWT_SECRET_KEY=os.getenv("JWT_SECRET_KEY")
